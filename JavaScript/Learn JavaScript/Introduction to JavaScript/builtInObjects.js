4e7981988a1f867026269c929d2634c412c354d9
Robert Carr <70494103+RobertCarrUTA@users.noreply.github.com>
2022-05-22 21:23:27 -0500

    Adding builtInObjects.js
    Just adding a file that shows how to use some of the JavaScript built-in objects.

---
+++ JavaScript/Learn JavaScript/Introduction to JavaScript/builtInObjects.js
@@ -0,0 +1,13 @@
+// Link to JavaScript Math documentation: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number
+
+// Inside of a console.log(), create a random number with Math.random(), then multiply it by 100.
+console.log(Math.random() * 100);
+
+// Now, use Math.floor() to make the output a whole number.
+console.log(Math.floor(Math.random() * 100));
+
+// Find a method on the JavaScript Math object that returns the smallest integer greater than or equal to a decimal number. <-- Math.ceil(x)
+console.log(Math.ceil(43.8));
+
+// Use the JavaScript documentation to find a method on the built-in Number object that checks if a number is an integer.
+console.log(Number.isInteger(2017));
