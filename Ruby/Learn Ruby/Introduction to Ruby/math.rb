289196aae8169e737be9f50203015e46cb47c881
Robert Carr <70494103+RobertCarrUTA@users.noreply.github.com>
2022-05-26 00:08:55 -0500

    Adding math.rb
    This file shows basic math in Ruby.

---
+++ Ruby/Learn Ruby/Introduction to Ruby/math.rb
@@ -0,0 +1,21 @@
+my_num = 100
+
+my_numAdd = my_num + my_num
+
+my_numSubract = my_num - my_num
+
+my_numMult = my_num * my_num
+
+my_numDiv = my_num / my_num
+
+my_numExp = my_num**
+
+my_numMod = my_num % my_num
+
+puts my_num
+puts my_numAdd
+puts my_numSubract
+puts my_numMult
+puts my_numDiv
+puts my_numExp
+puts my_numMod
